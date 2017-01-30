22bd5cade26b8caa34e12129231b4548f1ef7b01
joenelo <joeandrewnelson@gmail.com>
2017-01-30 12:15:05 -0800

    Started to Add Lizard (*not working yet) made a new Lizard Sprite Sheet Added body box (but when Nemo is jumping he is still floating up on branch when not full above it)

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-
-var game = new Phaser.Game(320, 500, Phaser.AUTO, '', { preload: preload, create: create , update: update});
+const LIZARD = 'lizard';
+const MOLE = 'mole';
+const BEE = 'bee';
+const CRAB = 'crab';
+const FISH = 'fish';
+const DRAGON = 'dragon';
 
 // Create my variables for...
 
@@ -9,19 +13,24 @@ var layer;
 //var background;
 var cursors;
 var hitButton;
+var enemies;
 
 
 // The player.
 var nemo;
+var lizard;
 
 
 
+
+var game = new Phaser.Game(320, 500, Phaser.AUTO, '', { preload: preload, create: create , update: update});
 
 
 function preload() {
     game.load.tilemap('level1', 'assets/nemo.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', 'assets/platformer_tiles.png');
     game.load.spritesheet('nemo', 'assets/nemo1.png', 48, 30);
+    game.load.spritesheet('LIZARD', 'assets/lizard.png', 21, 29);
     //game.load.image('background', 'assets/nemoBackground.png');
 
 
@@ -51,9 +60,7 @@ function create() {
     nemo.body.bounce.y = 0;
     nemo.body.linearDamping = 1;
     game.camera.follow(nemo);
-    // nemo.scale.x *= -1;
 
-    //nemo.sprite.scale.setTo(0.47, 0.47);
     nemo.anchor.x = 0.5;
     nemo.anchor.y = 0.5;
 
@@ -64,8 +71,36 @@ function create() {
     nemo.animations.add('duck', [1], 10, true);
     nemo.animations.add('hit', [11, 13, 14, 15, 16, 15, 14, 0], 20, true);
     nemo.body.fixedRotation = true;
-
     nemo.direction = 'right';
+    nemo.body.setSize(30, 20, 0, 10);
+
+
+    // enemies = game.add.group();
+    //
+    // //Lizard  sprite creation
+    // lizard = game.add.sprite(300, 3000, 'lizard');
+    // lizard.scale.setTo(0.47, 0.47);
+    // enemies.add(lizard, Phaser.Physics.ARCADE);
+    //
+    // // Lizard sprite physics
+    // lizard.body.bounce.y = 0;
+    // lizard.body.linearDamping = 1;
+    // lizard.body.collideWorldBounds = true;
+    //
+    // lizard.animations.add('stand', [0, 1, 2], 6, true);
+    // lizard.play('stand');
+    // lizard.body.velocity.x = 0;
+
+    // Mole
+
+
+    // Bee
+
+    // Crab
+
+    //Fish
+
+    //Dragon
 
     cursors = game.input.keyboard.createCursorKeys();
     hitButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -73,16 +108,15 @@ function create() {
     // Set collisions.
     map.setCollision([2, 3, 5, 41, 105, 106, 107, 108]);
 
-    // Create the enemy.
 }
 
 function update() {
 
     game.physics.arcade.collide(nemo, layer);
+    game.physics.arcade.collide(lizard, layer);
     // background.tilePosition.x = 0.5;
 
-   // make Nemo move
-
+   // Make Nemo move
     nemo.body.velocity.x = 0;
 
     if (cursors.left.isDown) {
